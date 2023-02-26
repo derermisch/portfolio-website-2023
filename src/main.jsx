5b411b8618111b6fca459425b2555a59d71e8330
derermisch <fabian.ermisch@googlemail.com>
2023-02-26 13:06:12 +0100

    mq test

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 import AppWrapper from './AppWrapper'
 import ScrollToTop from './components/general/ScrollToTop'
-import './css/main.css'
+import './css/main.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
